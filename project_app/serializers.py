e29f5cef3c0cad49e935826215b4d654e4622da0
Deepanwita2000 <85988268+Deepanwita2000@users.noreply.github.com>
2026-01-25 18:45:12 +0530

    Delete project_app directory

--- project_app/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Project
-
-class ProjecrSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Project
-        fields=['id','title','description','project_file','created_at','updated_at']
-
-  
